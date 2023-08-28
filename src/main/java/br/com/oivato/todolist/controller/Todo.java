f0f980b7fd2cd7b1b9715fab5bb76e8766bcdef3
Otávio Landim <otavio.landim@gmail.com>
2023-08-28 18:21:43 -0300

    Adding todoEntity

---
+++ src/main/java/br/com/oivato/todolist/controller/Todo.java
@@ -0,0 +1,65 @@
+package br.com.oivato.todolist.controller;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "todos")
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private boolean accomplished;
+    private int priority;
+
+    public Todo () {
+    }
+
+    public Todo (String name, String description, boolean accomplished, int priority) {
+        this.name = name;
+        this.description = description;
+        this.accomplished = accomplished;
+        this.priority = priority;
+    }
+
+    public Long getId () {
+        return id;
+    }
+
+    public void setId (Long id) {
+        this.id = id;
+    }
+
+    public String getName () {
+        return name;
+    }
+
+    public void setName (String name) {
+        this.name = name;
+    }
+
+    public String getDescription () {
+        return description;
+    }
+
+    public void setDescription (String description) {
+        this.description = description;
+    }
+
+    public boolean isAccomplished () {
+        return accomplished;
+    }
+
+    public void setAccomplished (boolean accomplished) {
+        this.accomplished = accomplished;
+    }
+
+    public int getPriority () {
+        return priority;
+    }
+
+    public void setPriority (int priority) {
+        this.priority = priority;
+    }
+}
