ddc6df0423cc8e35b3baba7ce833a0e7ac58f18a
Otávio Landim <otavio.landim@gmail.com>
2023-08-28 18:29:54 -0300

    Adding todoService

---
+++ src/main/java/br/com/oivato/todolist/service/TodoService.java
@@ -0,0 +1,35 @@
+package br.com.oivato.todolist.service;
+
+import br.com.oivato.todolist.entity.Todo;
+import br.com.oivato.todolist.repository.TodoRepository;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TodoService {
+
+    private TodoRepository todoRepository;
+
+    public List<Todo> create(Todo todo) {
+        todoRepository.save(todo);
+        return list();
+    }
+
+    public List<Todo> list() {
+        Sort sort = Sort.by(Sort.Direction.DESC, "priority")
+                .and(Sort.by(Sort.Direction.ASC, "id"));
+        return todoRepository.findAll(sort);
+    }
+
+    public List<Todo> update(Todo todo) {
+        todoRepository.save(todo);
+        return list();
+    }
+
+    public List<Todo> delete(Long id) {
+        todoRepository.deleteById(id);
+        return list();
+    }
+}
